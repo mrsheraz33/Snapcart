71fe67a9a4eef56d91e20830a60163bcbfcf6aa1
Muhammad Sheraz <mrsheraz325@gmail.com>
2026-08-18 22:25:17 +0500

    update files

--- src/app/api/user/payment/route.ts
+++ src/app/api/user/payment/route.ts
@@ -11,12 +11,14 @@ export async function POST(req: NextRequest) {
     await connectDB();
     const { userId, items, paymentMethod, totalAmount, address } =
       await req.json();
+
     if (!userId || !items || !paymentMethod || !totalAmount || !address) {
       return NextResponse.json(
         { message: "please send all data!" },
         { status: 400 },
       );
     }
+
     const user = await User.findById(userId);
     if (!user) {
       return NextResponse.json({ message: "user not found!" }, { status: 400 });
@@ -30,31 +32,42 @@ export async function POST(req: NextRequest) {
       address,
     });
 
+ 
+    const line_items = items.map((item: any) => {
+      const isPublicUrl =
+        item.image &&
+        (item.image.startsWith("http://") || item.image.startsWith("https://"));
+
+      return {
+        price_data: {
+          currency: "pkr",
+          product_data: {
+            name: item.name,
+            images: isPublicUrl ? [item.image] : [],
+          },
+          unit_amount: Math.round(Number(item.price) * 100), 
+        },
+        quantity: item.quantity || 1,
+      };
+    });
+
+   
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       mode: "payment",
       success_url: `${process.env.NEXT_BASE_URL}/user/order-success`,
       cancel_url: `${process.env.NEXT_BASE_URL}/user/order-cancel`,
-      line_items: [
-        {
-          price_data: {
-            currency: "pkr",
-            product_data: {
-              name: "SnapCart Order Payment",
-            },
-            unit_amount: Math.round(totalAmount * 100),
-          },
-          quantity: 1,
-        },
-      ],
+      line_items: line_items,
       metadata: {
         orderId: newOrder._id.toString(),
       },
     });
+
     return NextResponse.json({ url: session.url }, { status: 200 });
-  } catch (error) {
+  } catch (error: any) {
+    console.error("Stripe Checkout Error:", error);
     return NextResponse.json(
-      { message: `order payment error ${error}` },
+      { message: `order payment error: ${error?.message || error}` },
       { status: 500 },
     );
   }
